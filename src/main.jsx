3f321c8ff12fc3b8e79a16d640050742b0122537
agdev04 <dev@agnieve.com>
2025-01-03 21:57:43 +0800

    install analytics from next.js

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 console.log = function () {};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
+  <>
+  <SpeedInsights />
+  <Analytics /> 
   <App />
+  </>
 );
